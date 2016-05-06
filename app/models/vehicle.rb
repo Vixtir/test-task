32240188246e5b67a492c559becb9d735ebc9d8d
Paul <vixtir90@gmail.com>
2016-05-06 23:43:32 +0300

    boot

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -8,5 +8,7 @@ class Vehicle < ActiveRecord::Base
             :pts,
             :pts_date,
             presence: true
-	validates :gos_num, :pts, uniqueness: true    
+	validates :gos_num, :pts, uniqueness: true
+	validates_format_of :gos_num, :with => /\A[a-z]{1}\d{3}[a-z]{2}\s{1}\d{2}\z/i
+	validates_format_of :gos_num, :with => /\A\d{2}\s{1}[a-zA-Z]{2}\s\d{6}/i
 end
