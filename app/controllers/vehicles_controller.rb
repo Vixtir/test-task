50de97183516e44b065636554c8f6eba95833432
PAUL <vixtir90@gmail.com>
2016-05-11 15:26:37 +0300

    fixin

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,51 +1,51 @@
 class VehiclesController < ApplicationController
-	def index
-		@vehicles = Vehicle.all
+  def index
+    @vehicles = Vehicle.all
+  end
+
+  def show; end
+
+  def new
+    @vehicle = Vehicle.new
+    5.times { @vehicle.vehicle_attachments.build }
+  end
+
+  def create
+    @vehicle = Vehicle.create(vehicle_params)
+
+    if @vehicle.save
+      redirect_to root_path
+	else
+	  5.times { @vehicle.vehicle_attachments.build }
+	  render 'new'
+	  end
+  end
+
+  def edit
+    @vehicle = Vehicle.find(params[:id])
+	5.times { @vehicle.vehicle_attachments.build }
+  end
+
+  def update
+    @vehicle = Vehicle.find(params[:id])
+
+    if @vehicle.update(vehicle_params)
+ 	  redirect_to root_path
+	else
+	  render 'edit'
 	end
+  end
 
-	def show; end
+  def destroy
+	@vehicle = Vehicle.find(params[:id])
+	@vehicle.destroy
 
-	def new
-		@vehicle = Vehicle.new
-		5.times { @vehicle.vehicle_attachments.build }
-	end
-
-	def create
-		@vehicle = Vehicle.create(vehicle_params)
-
-		if @vehicle.save
-			redirect_to root_path
-		else
-			5.times { @vehicle.vehicle_attachments.build }
-			render 'new'
-		end
-	end
-
-	def edit
-		@vehicle = Vehicle.find(params[:id])
-		5.times { @vehicle.vehicle_attachments.build }
-	end
-
-	def update
-		@vehicle = Vehicle.find(params[:id])
-
-		if @vehicle.update(vehicle_params)
-			redirect_to root_path
-		else
-			render 'edit'
-		end
-	end
-
-	def destroy
-		@vehicle = Vehicle.find(params[:id])
-		@vehicle.destroy
-
-		redirect_to root_path
-	end
+	redirect_to root_path
+  end
 
 	private
 
-		def vehicle_params
-			params.require(:vehicle).permit(:name, :gos_num, :pts, :pts_date, vehicle_attachments_attributes: [:id, :vehicle_id, :image])
-		end
+	  def vehicle_params
+	    params.require(:vehicle).permit(:name, :gos_num, :pts, :pts_date, vehicle_attachments_attributes: [:id, :vehicle_id, :image])
+	  end
 end
