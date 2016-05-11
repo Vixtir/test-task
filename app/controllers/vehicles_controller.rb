16487516fe669c93a65331f94f3f7c28c0f8faa3
PAUL <vixtir90@gmail.com>
2016-05-11 15:30:25 +0300

    fixin

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -15,37 +15,37 @@ class VehiclesController < ApplicationController
 
     if @vehicle.save
       redirect_to root_path
-	else
-	  5.times { @vehicle.vehicle_attachments.build }
-	  render 'new'
-	  end
+    else
+      5.times { @vehicle.vehicle_attachments.build }
+      render 'new'
+    end
   end
 
   def edit
     @vehicle = Vehicle.find(params[:id])
-	5.times { @vehicle.vehicle_attachments.build }
+    5.times { @vehicle.vehicle_attachments.build }
   end
 
   def update
     @vehicle = Vehicle.find(params[:id])
 
     if @vehicle.update(vehicle_params)
- 	  redirect_to root_path
-	else
-	  render 'edit'
-	end
+      redirect_to root_path
+    else
+     render 'edit'
+    end
   end
 
   def destroy
-	@vehicle = Vehicle.find(params[:id])
-	@vehicle.destroy
+    @vehicle = Vehicle.find(params[:id])
+    @vehicle.destroy
 
-	redirect_to root_path
+    redirect_to root_path
   end
 
-	private
+  private
 
-	  def vehicle_params
-	    params.require(:vehicle).permit(:name, :gos_num, :pts, :pts_date, vehicle_attachments_attributes: [:id, :vehicle_id, :image])
-	  end
+    def vehicle_params
+      params.require(:vehicle).permit(:name, :gos_num, :pts, :pts_date, vehicle_attachments_attributes: [:id, :vehicle_id, :image])
+    end
 end
