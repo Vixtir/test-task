7c28639af3ed2ac8f4f3896b4682a39ac531ac98
Paul <vixtir90@gmail.com>
2016-05-14 14:45:24 +0300

    callback

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,4 +1,6 @@
 class Vehicle < ActiveRecord::Base
+  before_save :upcase
+
   has_many :vehicle_attachments, dependent: :destroy
   accepts_nested_attributes_for :vehicle_attachments, 
                                 allow_destroy: true
@@ -17,5 +19,10 @@ class Vehicle < ActiveRecord::Base
   validates :gos_num, :pts, uniqueness: true
 
   validates_format_of :gos_num, with: /\A[a-zA-Z]{1}\d{3}[a-zA-Z]{2}\s{1}\d{2,3}\z/i
-  validates_format_of :pts, with: /\A\d{2}\s{1}[а-яА-Я]{2}\s\d{6}/i
+  validates_format_of :pts, with: /\A\d{2}\s{1}[a-zA-Z]{2}\s\d{6}/i
+
+  def upcase
+    self.gos_num = gos_num.upcase
+    self.pts = pts.upcase
+  end
 end
